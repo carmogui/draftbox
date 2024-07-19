c406249d0d14c3ae7aeabc882cbb72a8a179f54c
guilherme.carmo <guilherme.carmo@cwi.com.br>
2024-07-19 14:34:24 -0300

    change the string typo in clean-string helper

--- src/pages/currency/helpers/clean-string/index.ts
+++ src/pages/currency/helpers/clean-string/index.ts
@@ -1,5 +1,5 @@
 function removeLastZero(val: string) {
-  const charToDeleteIndex = val.lastIndexOf("0");
+  const charToDeleteIndex = val.lastIndexOf('0');
 
   if (charToDeleteIndex + 1 === val.length) {
     return val.substring(0, charToDeleteIndex);
@@ -12,7 +12,7 @@ function removeLastZero(val: string) {
 }
 
 export function cleanString(val: string) {
-  const formattedValue = val.replace(/[^0-9]/g, "");
+  const formattedValue = val.replace(/[^0-9]/g, '');
   const valueWithoutLast0One = removeLastZero(formattedValue);
   const valueWithoutLast0Two = removeLastZero(valueWithoutLast0One);
 
